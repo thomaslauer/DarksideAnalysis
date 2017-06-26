7a4dc96878ba010a3ac907e2f6acf26c33ef2ffc
Thomas Lauer <thomaslauer@users.noreply.github.com>
2017-06-26 15:24:51 -0500

    Added file output

--- S2Fitter.h
+++ S2Fitter.h
@@ -7,13 +7,17 @@
 #include "TAxis.h"
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 
 using namespace std;
 
 class S2Fitter : public Module {
 
+    ofstream output;
+
     void init() {
+        output.open("residualInfo.txt");
     }
     
     TF1* runFit(TGraph* g) {
@@ -34,49 +38,67 @@ class S2Fitter : public Module {
         if(e.npulses > 4) {
             for(int droppedPulse = startPulse; droppedPulse < e.npulses; droppedPulse++) {
                 vector<double> x, y, tdrift;
-                for(int currentPulse = startPulse; currentPulse < e.npulses; currentPulse++) {
-                    // make sure all pulses reconstruct correctly
-                    bool goodXY = (e.pulse_x_masa[currentPulse] > -20)
-                                && (e.pulse_x_masa[currentPulse] < 20)
-                                && (e.pulse_y_masa[currentPulse] > -20)
-                                && (e.pulse_y_masa[currentPulse] < 20);
 
-                    if((currentPulse != droppedPulse) && goodXY) {
-                        x.push_back(e.pulse_x_masa[currentPulse]);
-                        y.push_back(e.pulse_y_masa[currentPulse]);
-                        tdrift.push_back(e.pulse_start_time[currentPulse] - e.pulse_start_time[0]);
+                bool droppedGoodXY = (e.pulse_x_masa[droppedPulse] > -20)
+                            && (e.pulse_x_masa[droppedPulse] < 20)
+                            && (e.pulse_y_masa[droppedPulse] > -20)
+                            && (e.pulse_y_masa[droppedPulse] < 20);
+                
+                if(droppedGoodXY) {
+                    for(int currentPulse = startPulse; currentPulse < e.npulses; currentPulse++) {
+                        // make sure all pulses reconstruct correctly
+                        bool currentGoodXY = (e.pulse_x_masa[currentPulse] > -20)
+                                    && (e.pulse_x_masa[currentPulse] < 20)
+                                    && (e.pulse_y_masa[currentPulse] > -20)
+                                    && (e.pulse_y_masa[currentPulse] < 20);
+
+                        if((currentPulse != droppedPulse) && currentGoodXY) {
+                            x.push_back(e.pulse_x_masa[currentPulse]);
+                            y.push_back(e.pulse_y_masa[currentPulse]);
+                            tdrift.push_back(e.pulse_start_time[currentPulse] - e.pulse_start_time[0]);
+                        }
                     }
+
+                    canvas->cd(1);
+                    TGraph* xvst = new TGraph(tdrift.size(), &tdrift[0], &x[0]);
+                    TF1* f1 = runFit(xvst);
+                    // (droppedPulse == startPulse) ? f1->Draw() : f1->Draw("same");
+
+                    if(droppedPulse == startPulse) {
+                        f1->Draw();
+                        f1->GetYaxis()->SetRangeUser(-18, 18);
+                    } else { 
+                        f1->Draw("same");
+                    }
+                    cout << "slope: " << f1->GetParameter(0) << " intercept: " << f1->GetParameter(1) << " residual: " 
+                            << e.pulse_x_masa[droppedPulse] - f1->Eval(e.pulse_start_time[droppedPulse] - e.pulse_start_time[0]) 
+                            << endl;
+
+                    output << e.pulse_start_time[droppedPulse] - e.pulse_start_time[0] << " "
+                            << e.pulse_x_masa[droppedPulse] << " "
+                            << e.pulse_y_masa[droppedPulse] << " "
+                            << f1->Eval(e.pulse_start_time[droppedPulse] - e.pulse_start_time[0]) << " ";
+
+                    canvas->cd(2);
+                    TGraph* yvst = new TGraph(tdrift.size(), &tdrift[0], &y[0]);
+                    TF1* f2 = runFit(yvst);
+                    if(droppedPulse == startPulse) {
+                        f2->Draw();
+                        f2->GetYaxis()->SetRangeUser(-18, 18);
+                    } else { 
+                        f2->Draw("same");
+                    }                
+                    cout << "slope: " << f2->GetParameter(0) << " intercept: " << f2->GetParameter(1) << " residual: " 
+                            << e.pulse_x_masa[droppedPulse] - f2->Eval(e.pulse_start_time[droppedPulse] - e.pulse_start_time[0]) 
+                            << endl;
+
+                    output << f2->Eval(e.pulse_start_time[droppedPulse] - e.pulse_start_time[0]) << endl;
                 }
-
-                canvas->cd(1);
-                TGraph* xvst = new TGraph(tdrift.size(), &tdrift[0], &x[0]);
-                TF1* f1 = runFit(xvst);
-                // (droppedPulse == startPulse) ? f1->Draw() : f1->Draw("same");
-
-                if(droppedPulse == startPulse) {
-                    f1->Draw();
-                    f1->GetYaxis()->SetRangeUser(-18, 18);
-                } else { 
-                    f1->Draw("same");
-                }
-                cout << "slope: " << f1->GetParameter(0) << " intercept: " << f1->GetParameter(1) << endl;
-
-                canvas->cd(2);
-                TGraph* yvst = new TGraph(tdrift.size(), &tdrift[0], &y[0]);
-                TF1* f2 = runFit(yvst);
-                if(droppedPulse == startPulse) {
-                    f2->Draw();
-                    f2->GetYaxis()->SetRangeUser(-18, 18);
-                } else { 
-                    f2->Draw("same");
-                }                
-                cout << "slope: " << f2->GetParameter(0) << " intercept: " << f2->GetParameter(1) << endl;
             }
         }
-
-
     }
 
     void cleanup() {
+        output.close();
     }
 };
