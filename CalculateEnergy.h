15f76b8ff8c3cc02a872eccba49ca089ec624ddd
Thomas Lauer <thomaslauer@users.noreply.github.com>
2017-07-20 15:44:58 -0500

    More changes

--- CalculateEnergy.h
+++ CalculateEnergy.h
@@ -7,12 +7,21 @@
 using namespace std;
 
 class CalculateEnergy : public Module {
-    
+public:
     TH1* henergy;
+    TH2* henergyvslsv;
+    TH1* f90;
+
+    bool mdraw;
+
+    CalculateEnergy(bool draw = false) {
+        mdraw = draw;
+    }
 
     void init() {
-        std::cout << "RUNNING INIT" << std::endl;
-        henergy = new TH1D("henergy", "energy", 10000, 0, 5000);
+        henergy = new TH1D("henergy", "energy", 10000, 0, 100000);
+        henergyvslsv = new TH2D("henergyvslsv", "energyvslsv", 1000, 0, 100000, 1000, 0, 200000);
+        f90 = new TH1D("f90", "f90", 100, 0, 1);
     }
     
     void processEvent(Event& e) {
@@ -26,11 +35,24 @@ class CalculateEnergy : public Module {
                 || (e.run_id > 15442 && e.run_id < 15620)
                 || (e.run_id > 15954 && e.run_id < 16066);
         
-
-        if(basicCuts && !runNumberCuts && e.npulses > 3 && e.total_f90>0.15 && e.s1>60&&e.tdrift>5&&e.tdrift<380&&e.has_s3==false) {
-            double energy = .0195*(e.correctedS1/0.154+e.correctedS2/35.34);
-            henergy->Fill(energy);
+        if(mdraw) cout << e.correctedS1 << " " << e.correctedS2 << endl;
+        
+        for(int i = 0; i < e.npulses; i++) {
+            f90->Fill(e.pulse_f90[i]);
         }
+
+        if(basicCuts && !runNumberCuts && e.npulses > 3) {
+            double energy = .0195*(e.s1/0.154+e.s2/35.34);
+            henergy->Fill(energy);
+            henergyvslsv->Fill(e.veto_lsv_charge, energy);
+            if(mdraw) cout << "Energy is " << energy << endl;
+        }
+
+        // if(basicCuts && !runNumberCuts && e.npulses > 3 && e.total_f90>0.1 && e.s1>60&&e.tdrift>5&&e.tdrift<380&&e.has_s3==false) {
+        //     double energy = .0195*(e.correctedS1/0.154+e.correctedS2/35.34);
+        //     henergy->Fill(energy);
+        //     if(mdraw) cout << "Energy is " << energy << endl;
+        // }
     }
 
     void cleanup() {
--- Main.cpp
+++ Main.cpp
@@ -1,25 +1,103 @@
 #include "darkside/Engine.h"
+#include "darkside/Module.h"
 
 #include "TFile.h"
 
 #include "darkside/SladLoader.h"
 #include "darkside/Module.h"
 #include "darkside/EnergyCorrections.h"
+#include "CalculateEnergy.h"
 
 #include "MuonRunNumbers/SaveEventFromSlad.h"
 
+#include <fstream>
+
+class npePerPulse : public Module {
+public:
+
+    float threshNPE = 100;
+
+    int xyAlgorithm = 0;
+
+    ofstream output;
+    void init() {
+        output.open("output/fullDetector.txt");
+    }
+
+    void processEvent(Event& e) {
+
+        vector<float> data_x;
+        vector<float> data_y;
+        vector<float> data_r;
+
+        if(xyAlgorithm == 0) {
+            data_x.assign(e.pulse_x_masa, e.pulse_x_masa + 100);
+            data_y.assign(e.pulse_y_masa, e.pulse_y_masa + 100);
+            data_r.assign(e.pulse_r_masa, e.pulse_r_masa + 100);
+        }
+        if(xyAlgorithm == 1) {
+            data_x.assign(e.pulse_x_jason, e.pulse_x_jason + 100);
+            data_y.assign(e.pulse_y_jason, e.pulse_y_jason + 100);
+            data_r.assign(e.pulse_r_jason, e.pulse_r_jason + 100);
+        }
+        if(xyAlgorithm == 2) {
+            data_x.assign(e.pulse_x_andrew, e.pulse_x_andrew + 100);
+            data_y.assign(e.pulse_y_andrew, e.pulse_y_andrew + 100);
+            data_r.assign(e.pulse_r_andrew, e.pulse_r_andrew + 100);
+        }
+
+        bool basicCuts = (e.nchannels == 38)
+                && (e.baseline_not_found == false)
+                && ((e.live_time + e.inhibit_time) >= 1.35e-3)
+                && (e.live_time < 1.);
+
+        bool filledFullDetector = true;
+        for(int i = 0; i < e.npulses; i++) {
+            if(e.pulse_total_npe[i] < threshNPE) {
+                filledFullDetector = false;
+                break;
+            }
+        }
+
+        int goodXY = 0;
+        for(int i = 0; i < e.npulses; i++) {
+            bool goodPulse = (data_x[i] > -20)
+                            && (data_x[i] < 20)
+                            && (data_y[i] > -20)
+                            && (data_y[i] < 20);
+            if(goodPulse) {
+                goodXY++;
+            }
+        }
+
+        if(filledFullDetector && basicCuts) {
+            output << Engine::getInstance()->getCurrentSladEvent() << " " << e.run_id << " " << e.event_id << " " << goodXY << endl;
+        }
+    }
+
+    void cleanup() {
+        output.close();
+    }
+
+};
+
 void Main() {
     Engine::init("~/SLAD/UAr_500d_SLAD_v2_3_3_merged_v0.root");
+    // Engine::init("~/SLAD/slad_muon_all.root");
+
     Engine* e = Engine::getInstance();
 
     e->slad->addSladFile("_allpulses.root", "pulse_info");
-    // e->slad->addSladFile("_masas_xy.root", "allpulses_xy");
-    // e->slad->addSladFile("_veto_cluster.root", "veto");
-    // e->slad->addSladFile("_xylocator_xy.root", "allpulses_xyl_xy");
-    // e->slad->addSladFile("_aww_xy.root", "allpulses_aww_xy");
+    e->slad->addSladFile("_masas_xy.root", "allpulses_xy");
+    e->slad->addSladFile("_veto_cluster.root", "veto");
+    e->slad->addSladFile("_xylocator_xy.root", "allpulses_xyl_xy");
+    e->slad->addSladFile("_aww_xy.root", "allpulses_aww_xy");
 
-    e->addModule(new SaveEventFromSlad());
+    // e->addModule(new EnergyCorrections());
+    e->addModule(new CalculateEnergy());
+    // e->addModule(new npePerPulse());
 
+    e->setOutput("output/energy.root");
 
-    Engine::getInstance()->run();
+    e->run();
 }
