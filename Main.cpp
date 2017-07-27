644587c6988e370bb4921a30918bc69544744622
Thomas Lauer <thomaslauer@users.noreply.github.com>
2017-07-27 08:56:10 -0500

    Reduced number of SLAD files for energy calculations

--- Main.cpp
+++ Main.cpp
@@ -89,9 +89,9 @@ void Main() {
 
     e->slad->addSladFile("_allpulses.root", "pulse_info");
     e->slad->addSladFile("_masas_xy.root", "allpulses_xy");
-    e->slad->addSladFile("_veto_cluster.root", "veto");
-    e->slad->addSladFile("_xylocator_xy.root", "allpulses_xyl_xy");
-    e->slad->addSladFile("_aww_xy.root", "allpulses_aww_xy");
+    // e->slad->addSladFile("_veto_cluster.root", "veto");
+    // e->slad->addSladFile("_xylocator_xy.root", "allpulses_xyl_xy");
+    // e->slad->addSladFile("_aww_xy.root", "allpulses_aww_xy");
 
     // e->addModule(new EnergyCorrections());
     e->addModule(new CalculateEnergy());
